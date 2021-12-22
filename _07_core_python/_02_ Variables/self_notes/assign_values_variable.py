f74d1ff491ada5418e0dabb35b001f03256a50d8
raajisuresh <bhimacharlarajeswari@gmail.com>
2021-12-22 16:53:13 +0530

    self notes and examples on variables

---
+++ _07_core_python/_02_ Variables/self_notes/assign_values_variable.py
@@ -0,0 +1,45 @@
+
+'''Assigning a single value to mmultiple variables
+a=b=c=d=5           #write operation
+print(a,b,c,d,id(a),id(b),id(c),id(d))  #read operation'''
+
+'''#Assigning multiple values to multiple variables
+x,y,z="raji",True,[1,2,3]
+print(x,id(x))
+print(y,id(y))
+print(z,id(z))
+print(id(z[0]))
+print(id(z[1]))
+print(id(x[0]))
+print(id(x[1]))
+#print(id(a),id(b))'''
+
+'''x, y, z = "Orange", "Banana", "Cherry"
+print(x,id(x))
+print(y,id(y))
+print(z,id(z))'''
+
+
+'''a=b=c="India"
+print(a,id(a))
+print(b,id(b))
+print(c,id(c))
+print(a[1],id(a[1]))
+print(b[1],id(b[1]))'''
+
+'''names=['hari','kishore','nandhan']
+n1,n2,n3=names
+print(n1)
+print(n2)
+print(n3)'''
+
+
+'''fruits = ["apple", "banana", "cherry"]
+p=q=r = fruits
+print(p,id(p))
+print(q,id(q))
+print(r,id(r))
+print(r[0],id(r[0]))
+print(p[0],id(p[0]))
+print(p[1],id(p[1]))
+print(r[1],id(r[1]))'''
