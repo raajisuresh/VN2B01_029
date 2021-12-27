0f859604e9a0c93afc31b65199de8000baf502ee
raajisuresh <bhimacharlarajeswari@gmail.com>
2021-12-27 17:12:58 +0530

    self notes

---
+++ _07_core_python/_4_Operators/self notes/operators_prog.py
@@ -0,0 +1,76 @@
+#arithmetic operators
+'''x=56
+y=25
+z=38
+a=5
+b=10
+add=x+y+z
+sub=x-y-z
+mul=x*y*z
+div=x/y
+mod=x%b
+exp=y**a
+f_div=x//a
+print(add,sub,mul,div,mod,exp,f_div)'''
+
+#assignment operators
+'''x1=12
+y1=9
+z1=24
+x2=x1+1
+y2=y1-1
+z2=z1*2
+x3=x2/2
+y3=y1%3
+z3=z1&z2
+x4=(x1>>1)
+y4=(x1<<1)
+print(x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4)'''
+
+#comparison operators
+'''a1=17
+b1=27
+a2=(a1==b1)
+b2=(a1!=b1)
+a3=(a1<b1)
+b3=(a1>b1)
+a4=(a1>=b1)
+b4=(a1<=b1)
+print(a1,b1,a2,b2,a3,b3,a4,b4)'''
+
+#logical operators
+'''p=15
+q=19
+p1=((p>10)and(q<p))
+q1=((p>q)or(p<10))
+p2=not(p==q)
+print(p,q,p1,q1,p2)'''
+
+
+#identity operators
+'''x=15
+y=17
+print(x is y)
+z=15
+print(x is z)
+print(x is not y)
+print(x is not z)'''
+
+
+#membership operators
+'''x=[1,2,3,4,100]
+print(1 in x)
+print(1000 in x)'''
+
+
+#bitwise operators
+'''x=78
+y=45
+print(x&y)
+print(x^y)
+print(~x)
+print(x<<y)
+print(x>>y)'''
+
+
+
